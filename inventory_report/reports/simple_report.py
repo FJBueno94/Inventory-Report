0728f7378ce4e60e20c44a343068d4127a0d324d
FJBueno94 <fernando_jordi@hotmail.com>
2023-01-11 18:22:57 -0300

    gera o relatório simples

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,26 @@
+import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    def generate(inventory):
+        fabricação_mais_antigo = min(
+            [item["data_de_fabricacao"] for item in inventory]
+        )
+
+        data = datetime.date.today().strftime("%d/%m/%Y")
+
+        validade_mais_novo = min(
+            [item["data_de_validade"] for item in inventory
+                if item["data_de_validade"] > data]
+        )
+
+        nome_fabrica = [empresa["nome_da_empresa"] for empresa in inventory]
+
+        empresa_produtos, _ = Counter(nome_fabrica).most_common()[0]
+
+        return (
+            f"Data de fabricação mais antiga: {fabricação_mais_antigo}\n"
+            f"Data de validade mais próxima: {validade_mais_novo}\n"
+            f"Empresa com mais produtos: {empresa_produtos}"
+        )
