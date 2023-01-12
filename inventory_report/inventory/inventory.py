caf58243b44b072c3e37cb0d0fbda94da83c2eaa
FJBueno94 <fernando_jordi@hotmail.com>
2023-01-12 18:17:46 -0300

    adiciona a leitura de arquivos json a classe inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,13 +1,18 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
     def import_data(path, type):
-        with open(path, "r") as file:
-            reader = csv.DictReader(file)
-            info = [row for row in reader]
+        if path.endswith(".csv"):
+            with open(path, "r") as file:
+                reader = csv.DictReader(file)
+                info = [row for row in reader]
+        elif path.endswith(".json"):
+            with open(path, "r") as file:
+                info = json.load(file)
 
         if type == "simples":
             return SimpleReport.generate(info)
