1496a558170071a49cd1eaedf5bdf64052fd8afe
FJBueno94 <fernando_jordi@hotmail.com>
2023-01-12 18:43:21 -0300

    adiciona a leitura de arquivos xml a classe inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
+import xmltodict
 
 
 class Inventory:
@@ -13,6 +14,10 @@ class Inventory:
         elif path.endswith(".json"):
             with open(path, "r") as file:
                 info = json.load(file)
+        else:
+            with open(path, "r") as file:
+                info = xmltodict.parse(file.read())
+                info = info["dataset"]["record"]
 
         if type == "simples":
             return SimpleReport.generate(info)
